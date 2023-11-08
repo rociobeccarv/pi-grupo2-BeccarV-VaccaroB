debf80696aa0174479e0a43dd2f3b3eb765dd370
rociobeccarv <rbeccarvarela@udesa.edu.ar>
2023-11-08 11:34:56 -0300

    test

---
+++ js/index.js
@@ -0,0 +1,7 @@
+let acaVaLaAPIKey = "1d1a4834e0e0c21ff813f10438647adb"
+let Mejorcalificadas = `https://api.themoviedb.org/3/movie/top_rated?api_key=${acaVaLaAPIKey}`
+
+let PelículasPopulares =`https://api.themoviedb.org/3/movie/popular?api_key=${acaVaLaAPIKey}`
+
+let SeriesPopulares = `https://api.themoviedb.org/3/tv/popular?api_key=${acaVaLaAPIKey}`
+
