e260b3e3fd3980241eab0906f69cd0c34eb4c393
nataliavaccarobianchi <nvaccarobianchi@udesa.edu.ar>
2023-11-12 16:02:27 -0300

    Commit temporal - cambios Natalia

--- js/index.js
+++ js/index.js
@@ -5,3 +5,26 @@ let PelículasPopulares =`https://api.themoviedb.org/3/movie/popular?api_key=${a
 
 let SeriesPopulares = `https://api.themoviedb.org/3/tv/popular?api_key=${acaVaLaAPIKey}`
 
+
+let menuBtn = document.querySelector('#menuBtn');
+let menuOptions = document.querySelector('#menuOptions');
+let searchForm = document.querySelector('.searchForm');
+
+menuBtn.addEventListener('click', function (event) {
+    event.stopPropagation(); // Evita que el clic en el botón se propague al documento
+    menuOptions.style.display = (menuOptions.style.display === 'none') ? 'block' : 'none';
+
+});
+
+    // Cierra el menú si se hace clic fuera de él
+document.addEventListener('click', function () {
+        menuOptions.style.display = 'none';
+});
+
+
+// Cierra el menú si se desplaza la ventana
+window.addEventListener('scroll', function () {
+    menuOptions.style.display = 'none';
+});
+
+
