7581ff689b41266cc46e859b5becbae5b37b6cd8
nataliavaccarobianchi <nvaccarobianchi@udesa.edu.ar>
2023-11-17 18:27:02 -0300

    otro merge nc pq

--- js/genero.js
+++ js/genero.js
@@ -2,8 +2,10 @@ let qs = location.search;
 let qsObj = new URLSearchParams(qs);
 let idGenero = qsObj.get("idGenero");
 let acaVaLaAPIKey = "1d1a4834e0e0c21ff813f10438647adb";
-let generosP = `https://api.themoviedb.org/3/discover/movie?api_key=${acaVaLaAPIKey}&with_genres=$(id_genero)`;
-let tituloGenero = document.querySelector(".tituloGenero")
+let generosP = `https://api.themoviedb.org/3/discover/movie?api_key=${acaVaLaAPIKey}&with_genres=${idGenero}`;
+let generosS =`https://api.themoviedb.org/3/discover/tv?api_key=${acaVaLaAPIKey}&with_genres=${idGenero}`;
+let Peliculas = document.querySelector(".GenerosP");
+let Series = document.querySelector(".GenerosS");
 
 fetch(generosP)
 .then(function(response) {
@@ -13,12 +15,14 @@ fetch(generosP)
     console.log(data)
     
     
-    let contenido = "";
+    let contenido = "<h2>PELICULAS: </h2>";
     for (let i = 0; i < 11; i++) {
         let pelicula = data.results[i]
 
         let poster = `https://image.tmdb.org/t/p/w500/${pelicula.poster_path}`
-        contenido += `<div>
+        contenido += 
+        `
+        <div>
         <a href="./PeliculaDetalle.html?idPelicula=${pelicula.id}"  rel="noopener noreferrer">
                 <img src=${poster} alt="avatar" height="300px" width = "210">
             </a> 
@@ -28,7 +32,41 @@ fetch(generosP)
         
     }
 
-    tituloGenero.innerHTML = contenido;
+    Peliculas.innerHTML = contenido;
+
+   
+
+  
+})
+.catch(function(error) {
+    console.log(error);
+});
+fetch(generosS)
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    console.log(data)
+    
+    
+    let contenido = "<h2>SERIES: </h2>";
+    for (let i = 0; i < 11; i++) {
+        let pelicula = data.results[i]
+
+        let poster = `https://image.tmdb.org/t/p/w500/${pelicula.poster_path}`
+        contenido += 
+        `
+        <div>
+        <a href="./PeliculaDetalle.html?idPelicula=${pelicula.id}"  rel="noopener noreferrer">
+                <img src=${poster} alt="avatar" height="300px" width = "210">
+            </a> 
+            ${pelicula.name} (${pelicula.first_air_date})
+        </div><br/>` 
+        
+        
+    }
+
+    Series.innerHTML = contenido;
 
    
 
--- js/generos.js
+++ js/generos.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let acaVaLaAPIKey = "1d1a4834e0e0c21ff813f10438647adb";
 let pelis = `https://api.themoviedb.org/3/genre/movie/list?api_key=${acaVaLaAPIKey}`;
 let serie = `https://api.themoviedb.org/3/genre/tv/list?api_key=${acaVaLaAPIKey}`;
@@ -10,13 +9,14 @@ fetch(pelis)
     return response.json();
 })
 .then(function(data) {
-    console.log(data)
+   
+    let genero = data.genres
     let contenido = "";
-
-    for (let i = 0; i < pelis.length + 1; i++) {
-        let genero = data.genres[i];
-        
-        contenido += `<div class="genero"><a href="./genero.html?idGenero=${genero.id}"  rel="noopener noreferrer"> ${genero} </a></div>`
+    for (let i = 0; i < pelis.length ; i++) {
+        let type = genero[i]
+        if (type !== undefined){
+        contenido += `<div class="genero"><a href="./genero.html?idGenero=(id:${type.id},name:${type.name})"  rel="noopener noreferrer"> ${type.name} </a></div>`
+        }
         
         
     }
@@ -31,65 +31,27 @@ fetch(pelis)
 
 fetch(serie)
 .then(function(response) {
-    return response.json();
-})
-.then(function(data) {
-    console.log(data)
-    let contenido = "";
+     return response.json();
+ })
+ .then(function(data) {
+     
+     let genero = data.genres
+     let contenido = "";
 
-    for (let i = 0; i < serie.length + 1; i++) {
-        let genero = data.genres[i];
-        
-       contenido += `<div class="genero"> ${genero} </div>`
+     for (let i = 0; i < serie.length ; i++) {
+        let type = genero[i]
+        if (type !== undefined){
+        contenido += `<div class="genero"><a href="./genero.html?idGenero=${type.id}"  rel="noopener noreferrer"> ${type.name} </a></div>`
+        }
         
         
     }
     
-    series.innerHTML = contenido;
+    
+     series.innerHTML = contenido;
 
   
-})
-.catch(function(error) {
-    console.log(error);
-});
-=======
-
-let acaVaLaAPIKey = "1d1a4834e0e0c21ff813f10438647adb"
-
-let Mejorcalificadas = `https://api.themoviedb.org/3/movie/top_rated?api_key=${acaVaLaAPIKey}`
-
-let PelículasPopulares =`https://api.themoviedb.org/3/movie/popular?api_key=${acaVaLaAPIKey}`
-
-let SeriesPopulares = `https://api.themoviedb.org/3/tv/popular?api_key=${acaVaLaAPIKey}`
-
-let menuBtn = document.querySelector('#menuBtn');
-let menuOptions = document.querySelector('#menuOptions');
-let searchForm = document.querySelector('.searchForm');
-
-let listaMejores = document.querySelector("#mejores");
-let listaPopulares = document.querySelector("#populares");
-let listaSeries = document.querySelector("#series");
-const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer 1d1a4834e0e0c21ff813f10438647adb'
-    }
-    };
-  fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
-    const options = {
-        method: 'GET',
-        headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer 1d1a4834e0e0c21ff813f10438647adb'
-        }
-    }
-      
-fetch('https://api.themoviedb.org/3/genre/tv/list?language=en', options)
-.then(response => response.json())
-.then(response => console.log(response))
-.catch(err => console.error(err))
->>>>>>> 2541ea4bbc5cf9610d893f774190db77f20677a7
+ })
+ .catch(function(error) {
+     console.log(error);
+ });
