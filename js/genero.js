848aa3fc0b2d8bb3f39c8952146a608bd1b493e1
nataliavaccarobianchi <142626606+nataliavaccarobianchi@users.noreply.github.com>
2023-11-21 09:54:59 -0300

    electivas

--- js/genero.js
+++ js/genero.js
@@ -3,75 +3,75 @@ let qsObj = new URLSearchParams(qs);
 let idGenero = qsObj.get("idGenero");
 let acaVaLaAPIKey = "1d1a4834e0e0c21ff813f10438647adb";
 let generosP = `https://api.themoviedb.org/3/discover/movie?api_key=${acaVaLaAPIKey}&with_genres=${idGenero}`;
-let generosS =`https://api.themoviedb.org/3/discover/tv?api_key=${acaVaLaAPIKey}&with_genres=${idGenero}`;
+let generosS = `https://api.themoviedb.org/3/discover/tv?api_key=${acaVaLaAPIKey}&with_genres=${idGenero}`;
 let Peliculas = document.querySelector(".GenerosP");
 let Series = document.querySelector(".GenerosS");
 
 fetch(generosP)
-.then(function(response) {
-    return response.json();
-})
-.then(function(data) {
-    console.log(data)
-    
-    
-    let contenido = "<h2>PELICULAS: </h2>";
-    for (let i = 0; i < 11; i++) {
-        let pelicula = data.results[i]
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
 
-        let poster = `https://image.tmdb.org/t/p/w500/${pelicula.poster_path}`
-        contenido += 
-        `
+
+        let contenido = "";
+        for (let i = 0; i < 11; i++) {
+            let pelicula = data.results[i]
+
+            let poster = `https://image.tmdb.org/t/p/w500/${pelicula.poster_path}`
+            contenido +=
+                `
         <div>
         <a href="./PeliculaDetalle.html?idPelicula=${pelicula.id}"  rel="noopener noreferrer">
                 <img src=${poster} alt="avatar" height="300px" width = "210">
             </a> 
             ${pelicula.original_title} (${pelicula.release_date})
-        </div><br/>` 
-        
-        
-    }
+        </div><br/>`
 
-    Peliculas.innerHTML = contenido;
 
-   
+        }
 
-  
-})
-.catch(function(error) {
-    console.log(error);
-});
+        Peliculas.innerHTML = contenido;
+
+
+
+
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
 fetch(generosS)
-.then(function(response) {
-    return response.json();
-})
-.then(function(data) {
-    console.log(data)
-    
-    
-    let contenido = "<h2>SERIES: </h2>";
-    for (let i = 0; i < 11; i++) {
-        let pelicula = data.results[i]
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
 
-        let poster = `https://image.tmdb.org/t/p/w500/${pelicula.poster_path}`
-        contenido += 
-        `
+
+        let contenido = "";
+        for (let i = 0; i < 11; i++) {
+            let pelicula = data.results[i]
+
+            let poster = `https://image.tmdb.org/t/p/w500/${pelicula.poster_path}`
+            contenido +=
+                `
         <div>
         <a href="./PeliculaDetalle.html?idPelicula=${pelicula.id}"  rel="noopener noreferrer">
                 <img src=${poster} alt="avatar" height="300px" width = "210">
             </a> 
             ${pelicula.name} (${pelicula.first_air_date})
-        </div><br/>` 
-        
-        
-    }
+        </div><br/>`
 
-    Series.innerHTML = contenido;
 
-   
+        }
 
-  
-})
-.catch(function(error) {
-    console.log(error);
-});
+        Series.innerHTML = contenido;
+
+
+
+
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
